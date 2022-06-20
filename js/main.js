3fa1f97d445583a81d91c53570d9e897bed58c0c
TatianaKhoroshilova <107188157+TatianaKhoroshilova@users.noreply.github.com>
2022-06-20 15:28:19 +0300

    функции и main js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Source: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+// имя_функции(проверяемая_строка, максимальная_длина);
+// Результат: true, если строка проходит по длине, и false — если не проходит
+let comment  =
+let commentLength = comment.length
+function checkCommentLength (commentLength, maxLength) {
+if (commentLength > maxLength) {console.log ('Длина комментария не может составлять больше 140 символов ')}
+} else {
+   console.log (comment)
+};
+checkCommentLength ();

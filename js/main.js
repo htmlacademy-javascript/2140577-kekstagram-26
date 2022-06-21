20f7a7678e4b622a9ee16b1cebcddf4ff824ba1f
TatianaKhoroshilova <107188157+TatianaKhoroshilova@users.noreply.github.com>
2022-06-21 19:12:12 +0300

    доп.исправление

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function getRandomIntInclusive(min, max) {
   const minNumber = Math.ceil(min);
   const maxNumber = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 }
 getRandomIntInclusive ();
 

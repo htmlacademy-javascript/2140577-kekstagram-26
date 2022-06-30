30ca8e061251ce578b7afa5ff167fca1775e9bf5
TatianaKhoroshilova <107188157+TatianaKhoroshilova@users.noreply.github.com>
2022-06-30 18:15:56 +0300

    module4-task1* upd

--- js/main.js
+++ js/main.js
@@ -3,23 +3,25 @@ function getRandomIntInclusive(min, max) {
   const maxNumber = Math.floor(max);
   return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 }
-getRandomIntInclusive ();
 
 function checkCommentLength (commentLength, maxLength) {
-  if (commentLength > maxLength)
-  {console.log ('Длина комментария не может составлять больше 140 символов ');}
-  else {
-    console.log ('ok');}
+  if (commentLength > maxLength) {
+    console.log ('Длина комментария не может составлять больше 140 символов ');
+  } else  {
+    console.log ('ok');
+  }
 }
-checkCommentLength ();
+function getRandomArrayElement (arr) {
+  return arr[getRandomIntInclusive (0, arr.length+1)]
+}
 
- function getIdList (number) {
+function getIdList (number) {
   return Array (number)
   .fill (0)
   .map((_, idx) => idx + 1);
 }
-const ID = getIdList(25);
-const URL = Array.from({length:25}, (value, index) => `photos/${getRandomIntInclusive(1, 25)}.jpg`);
+const id = getIdList(25);
+const url = Array.from({length:25}, (value, index) => `photos/${getRandomIntInclusive(1, 25)}.jpg`);
 const DESCRIPTIONS = [
 'На отдыхе',
 'В целом всё норм',
@@ -27,7 +29,7 @@ const DESCRIPTIONS = [
 'Хорошо, что лето',
 'Речные прогулки'
 ];
-const LIKES = getIdList (200);
+const likes = getIdList (200);
 const MESSAGE = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -36,7 +38,7 @@ const MESSAGE = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-const AVATAR = Array.from ({length:6}, (value, index) => `img/avatar-${getRandomIntInclusive(1, 6)}.svg`);
+const avatar= Array.from ({length:6}, (value, index) => `img/avatar-${getRandomIntInclusive(1, 6)}.svg`);
 const NAMES = [
   'Иван',
   'Анна',
@@ -48,17 +50,14 @@ const NAMES = [
   'Антон',
   'Мэри'
 ];
-const getPhotoDescription =()=> {
- return {
-   id: ID[getRandomIntInclusive (0, ID.length+1)],
-   avatar: AVATAR[getRandomIntInclusive(0, AVATAR.length+1)],
-   message: MESSAGE[getRandomIntInclusive(0, MESSAGE.length+1)],
-   name: NAMES[getRandomIntInclusive(0, NAMES.length+1)]
- };
-};
+const getPhotoDescription =()=>
+  ({
+    id: getRandomArrayElement(id),
+    avatar:getRandomArrayElement (avatar),
+    message: getRandomArrayElement (MESSAGE),
+    name: getRandomArrayElement (NAMES)
+  });
 
-console.log (
-  getPhotoDescription()
-);
+
 const similarPhotoDescription = Array.from({length: 25}, getPhotoDescription);
 console.log (similarPhotoDescription);
